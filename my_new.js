f8756d7bdbd12e4d3f0a2a9ac0c1c9069be424dc
Swathisree <swathisree@gowdanar.com>
2020-03-04 12:40:05 +0530

    initial commit

---
+++ my_new.js
@@ -0,0 +1,15 @@
+
+<h1>Creating a javascript object</h1>
+<p id = "demo"></p>
+
+<script>
+
+var person = {
+	firstName : "Madhu",
+	lastName : "Reddy",
+	age : 25,
+	eyecolor : "Blue"
+};
+document.grtElementByid("demo").innerHTML = person.firstName + "is" + person.age + "years old";
+
+</script>
